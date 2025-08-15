4b45f47812f4f15399f5453dda233f88a600a625
Supa <36031171+0Supa@users.noreply.github.com>
2025-08-15 06:22:20 +0300

    part channels every 10 minutes

--- src/bot.rs
+++ src/bot.rs
@@ -201,13 +201,9 @@ impl Bot {
         let part_client = client.clone();
         tokio::spawn(async move {
             loop {
-                sleep(Duration::from_secs(10)).await;
+                sleep(Duration::from_secs(600)).await;
 
-                let mut queue = parting_queue.lock().unwrap();
-                let old_channels: Vec<_> = queue.iter().take(20).cloned().collect();
-
-                for channel in old_channels {
-                    queue.remove(&channel);
+                for channel in parting_queue.lock().unwrap().drain() {
                     part_client.part(channel);
                 }
             }
